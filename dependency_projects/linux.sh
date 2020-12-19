aede7ef9ec4a6395099d82f5d9d52d008610beb5
Gilles <gsouton@gmail.com>
2020-12-19 14:00:38 +0100

    package to install for project

---
+++ dependency_projects/linux.sh
@@ -0,0 +1,4 @@
+sudo apt-get -y install cmake
+
+
+sudo apt install libsdl2-mixer-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-gfx-dev
---
+++ dependency_projects/mac.sh
@@ -0,0 +1,7 @@
+brew install cmake
+
+brew install SDL2
+
+brew install SDL2_image
+
+brew install SDL2_ttf
